9113f30d9180bf9c5ca88037212d74d2a57b387c
nonphatic <afnl686@gmail.com>
2017-12-06 20:38:38 -0800

    Day 5 - replaced Seq Int with IntMap Int for slight performance improvement...

--- 5.hs
+++ 5.hs
@@ -1,24 +1,27 @@
-import Data.Sequence
+import Data.IntMap (IntMap, insert, size, fromList, findWithDefault)
 
+type Length = Int
 type Index = Int
 type Steps = Int
-type State = (Steps, Index, Seq Int)
+type State = (Steps, Index, IntMap Int)
 type Update = Int -> Int
 
 next :: Update -> State -> State
 next f (steps, i, jumps) =
-    let value = index jumps i
+    let value = findWithDefault undefined i jumps
         nextI = i + value
-        nextJumps = update i (f value) jumps
+        nextJumps = insert i (f value) jumps
     in  (steps + 1, nextI, nextJumps)
 
-getExitSteps :: Update -> State -> Int
-getExitSteps f (steps, i, jumps) =
-    if i >= Data.Sequence.length jumps then steps else getExitSteps f $ next f (steps, i, jumps)
+getExitSteps :: Length -> Update -> State -> Int
+getExitSteps len f (steps, i, jumps) =
+    if i >= len then steps else getExitSteps len f $ next f (steps, i, jumps)
 
 main :: IO ()
 main = do
     input <- readFile "5.txt"
-    let jumps = fromList . map read $ lines input :: Seq Int
-    print $ getExitSteps (+1)                                    (0, 0, jumps)
-    print $ getExitSteps (\v -> if v >= 3 then v - 1 else v + 1) (0, 0, jumps)
+    let jumpsList = map read $ lines input
+        jumpsMap  = fromList $ zip [0..] jumpsList
+        len = length jumpsList
+    print $ getExitSteps len (+1)                                    (0, 0, jumpsMap)
+    print $ getExitSteps len (\v -> if v >= 3 then v - 1 else v + 1) (0, 0, jumpsMap)
