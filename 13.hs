7a209658f3e91b5afa8dbdf6322aa9a2bec41db0
nonphatic <afnl686@gmail.com>
2017-12-12 22:59:14 -0800

    Day 13

---
+++ 13.hs
@@ -0,0 +1,29 @@
+import Data.List.Split (splitOn)
+import Data.List (findIndex)
+
+type Delay = Int
+type Firewall = (Int, Int)
+(%) = mod
+
+parseLine :: String -> Firewall
+parseLine str =
+    let depth : range : _ = splitOn ": " str
+    in  (read depth, read range)
+
+caught :: Delay -> Firewall -> Bool
+caught delay (depth, range) =
+    (depth + delay) % (2 * (range - 1)) == 0
+
+severity :: [Firewall] -> Int
+severity firewalls =
+    sum . map (\firewall -> uncurry (*) firewall * fromEnum (caught 0 firewall)) $ firewalls
+
+anyCaught :: [Firewall] -> Delay -> Bool
+anyCaught firewalls delay =
+    or  . map (caught delay) $ firewalls
+
+main :: IO ()
+main = do
+    firewalls <- fmap (map parseLine . lines) $ readFile "13.txt"
+    print $ severity firewalls
+    print $ findIndex not $ map (anyCaught firewalls) [0..]
