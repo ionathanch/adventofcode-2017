382cfba83e57eecbd634c602520dc7a0018bce4a
nonphatic <afnl686@gmail.com>
2017-12-15 09:28:02 -0800

    Day 15 - refactored strict version with some fancy ~~custom operators~~

--- 15_strict.hs
+++ 15_strict.hs
@@ -1,18 +1,23 @@
 {-# LANGUAGE BangPatterns #-}
 import Data.Function (on)
 import Data.Bits ((.&.))
+
+divisor = 2147483647
+factors = (16807, 48271)
+seed    = (634, 301)
         
-count :: Int -> (Int, Int) -> (Int, Int) -> (Int, Int) -> Int -> Int -> Int
-count divisor (factorA, factorB) (seedA, seedB) (divA, divB) acc times =
+count :: (Int, Int) -> (Int, Int) -> Int -> Int -> Int
+count pair masks acc times =
     if times == 0 then acc else
-    let !nextA = nextMaskBy factorA divA seedA
-        !nextB = nextMaskBy factorB divB seedB
-        !eq = fromEnum $ ((==) `on` (.&. 0xffff)) nextA nextB
-    in count divisor (factorA, factorB) (nextA, nextB) (divA, divB) (acc + eq) (times - 1)
-    where 
-        nextMaskBy f d s = let t = (f * s) `mod` divisor in if (t .&. d) == 0 then t else nextMaskBy f d t
+    let !next = nextMaskBy <$$> factors <**> masks <**> pair
+        !eq   = fromEnum . uncurry ((==) `on` (.&. 0xffff)) $ next
+    in count next masks (acc + eq) (times - 1)
+    where
+        h      <$$> (x, y) = (h x, h y)
+        (f, g) <**> (x, y) = (f x, g y)
+        nextMaskBy f d s  = let t = (f * s) `mod` divisor in if (t .&. d) == 0 then t else nextMaskBy f d t
 
 main :: IO ()
 main = do
-    print $ count 2147483647 (16807, 48271) (634, 301) (0, 0) 0 40000000
-    print $ count 2147483647 (16807, 48271) (634, 301) (3, 7) 0 5000000
+    print $ count seed (0, 0) 0 40000000
+    print $ count seed (3, 7) 0 5000000
